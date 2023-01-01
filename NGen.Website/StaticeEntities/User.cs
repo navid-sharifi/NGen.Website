70beccc9de5945448778bf5d2462af83cdc013ab
Navid <navid0523@gmail.com>
2023-01-01 15:56:34 +0330

    design new module

--- NGen.Website/StaticeEntities/User.cs
+++ NGen.Website/StaticeEntities/User.cs
@@ -5,5 +5,27 @@
         public string Name { get; set; }
         public string Password { get; set; }
         public string Roles { get; set; }
+		public string Family { get; set; }
+		public string Description { get; set; }
+	}
+    
+    public partial class File : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public long CreateDateTime { get; set; }
+        public User Creator { get; set; }
+        public Guid CreatorId { get; set; }
+
     }
+    public partial class Folder : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+		public long CreateDateTime { get; set; }
+		public User Creator { get; set; }
+		public Guid CreatorId { get; set; }
+		public Folder? Father { get; set; }
+		public Guid? FatherId { get; set; }
+	}
 }
